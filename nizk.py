e5a96d3ad5f9275acfdad801c5001863bd20888b
Guy1m0 <zjc.kingdom@gmail.com>
2024-04-11 22:08:25 +0800

    finish nizk used for pks

--- nizk.py
+++ nizk.py
@@ -14,124 +14,114 @@ class NIZK():
         #print ("add 2:", a)
         u = a + c * rps.s
         return R, u
-    
-    def commit_gA(self, pp, s, h):
+      
+    def commit_pk(self, pp, pk, sk):
         g_A = pp['g_1^A']
-        a = self.group.random(ZR)
-        #a = 1
-        R = []
-        k = len(g_A)
-        for i in range(k):
-            R.append(g_A[i] ** a)
-        R.append(pp['g_1']**a)
-
-        hash_input = str(R) + h
-        c = self.group.hash(hash_input, ZR)
-        u = []
-        # test
-        # c = 1
-        for i in range(k + 1):
-            y = []
-            for j in range(k + 1):
-                y.append(a + c * s[i][j])
-            u.append(y)
-
-        return R, u
-    
-    def commitPK(self, pp, pk, sk):
         e_gh = pp['e_g1_g2']
-        g_A = pp['g_1^A']
         k = len(g_A)
-        a = self.group.random(ZR)
-        #a = 0
 
-        #A = [pair(x, pp['g_2']) for x in g_A].append(e_gh)
-        R = [] #A
-        A = []
+        a1 = self.group.random(ZR)
+        a2 = self.group.random(ZR)
+        a3 = self.group.random(ZR)
+        R1, R2, u1,u2, A2, hash_inputs = ([] for i in range(6))
+        
         for i in range(k):
-            tmp = pair(g_A[i],pp['g_2'])
-            A.append(tmp)
-            R.append(tmp**a)
-        A.append(e_gh)
-        R.append(e_gh ** a)
+            R1.append(g_A[i] ** a1)
+            tmp = pair(g_A[i], pp['g_2'])
+            A2.append(tmp)
+            R2.append(tmp**a2)
+        R1.append(pp['g_1']**a1)
+        A2.append(e_gh)
+        R2.append(e_gh ** a2)
 
-        B = pk['e(g_1,g_2)^{tau^T A}']
-        s = sk['tau']
+        A1 = g_A
+        B1 = pk['g_1^{X^T A}']
+        s1 = sk['X']
+        B2 = pk['e(g_1,g_2)^{tau^T A}']
+        s2 = sk['tau']
+        A3 = pp['g_2']
+        B3 = pk['y']
+        R3 = A3 ** a3
+        s3 = sk['sigma']
 
-        h_x = self.group.hash([A,B], ZR)
-        #h_tau = self.group.hash(sk['tau'])
-        #h_sig = self.group.hash(sk['sigma'])
-        #h = h_x * h_tau * h_sig
+        h_x = self.group.hash([A1,B1], ZR)
+        h_tau = self.group.hash([A2,B2], ZR)
+        h_sig = self.group.hash([A3,B3], ZR)
+        h = h_x * h_tau * h_sig
 
-        hash_input = str(R) + str(h_x)
-        c = self.group.hash(hash_input, ZR)
-        #c = 1
-        #print ('c:', c)
-        u = []
+        hash_inputs.append(str(h) + str(h_x))
+        hash_inputs.append(str(h) + str(h_tau))
+        hash_inputs.append(str(h) + str(h_sig))
+
+        c1 = self.group.hash(hash_inputs[0], ZR)
+        c2 = self.group.hash(hash_inputs[1], ZR)
+        c3 = self.group.hash(hash_inputs[2], ZR)
+
         for i in range(k + 1):
-            u.append(a + c * s[i])
-        #u = [a + c * x for x in s]
+            y1 = []
+            for j in range(k + 1):
+                y1.append(a1 + c1 * s1[i][j])
+            u1.append(y1)
+            u2.append(a2+c2 * s2[i])
+        u3 = a3 + c3 * s3
 
-        return R, u
+        pi1 = {'R': R1, 'u': u1}
+        pi2 = {'R': R2, 'u': u2}
+        pi3 = {'R': R3, 'u': u3}
+        pis = {'X': pi1, 'tau': pi2, 'sig': pi3}
+
+        rps1 = {'A': A1, 'B': B1}
+        rps2 = {'A': A2, 'B': B2}
+        rps3 = {'A': A3, 'B': B3}
+        s_pairs = {'X': rps1, 'tau': rps2, 'sig': rps3}
+
+        return s_pairs, pis
+
+    def verify_pk(self, pp, s_pairs, pis):
+        g_A = pp['g_1^A']
+        e_gh = pp['e_g1_g2']
+        k = len(g_A)
+        hash_inputs = []
+
+        h_x = self.group.hash([s_pairs['X']['A'], s_pairs['X']['B']], ZR)
+        h_tau = self.group.hash([s_pairs['tau']['A'], s_pairs['tau']['B']], ZR)
+        h_sig = self.group.hash([s_pairs['sig']['A'], s_pairs['sig']['B']], ZR)
+        h = h_x * h_tau * h_sig
+
+        hash_inputs.append(str(h) + str(h_x))
+        hash_inputs.append(str(h) + str(h_tau))
+        hash_inputs.append(str(h) + str(h_sig))
+
+        c1 = self.group.hash(hash_inputs[0], ZR)
+        c2 = self.group.hash(hash_inputs[1], ZR)
+        c3 = self.group.hash(hash_inputs[2], ZR)
+
+        for j1 in range(k + 1):
+            for j2 in range(k):
+                eq1 = g_A[j2]**pis['X']['u'][j2][j1] * pp['g_1'] ** pis['X']['u'][k][j1]
+                eq2 = (s_pairs['X']['B'][j1][j2] ** c1) *  pis['X']['R'][j2] * pis['X']['R'][-1]
+                if eq1 != eq2:# * R[j1]:
+                    print ("False for X in index: ", j1, j2)
+                    return False
+                
+        for i in range(k):
+            eq1 = s_pairs['tau']['A'][i] ** pis['tau']['u'][i] * e_gh ** pis['tau']['u'][k]
+            eq2 = s_pairs['tau']['B'][i] ** c2 * pis['tau']['R'][i] * pis['tau']['R'][k]
+            if eq1 != eq2:
+                print ("False for tau in index:", i)
+                return False
+            
+        return s_pairs['sig']['A'] ** pis['sig']['u'] == pis['sig']['R'] * (s_pairs['sig']['B'] ** c3)
+
     
     def verify(self, rps, R, u, h):
         hash_input = str(R) + h
         c = self.group.hash(hash_input, ZR)
 
         return rps.A ** u == R * (rps.B ** c)
-    
-    def verify_gA(self, pp, B, R, u, h):
-        g_A = pp['g_1^A'] #A
-        g = pp['g_1']
-        hash_input = str(R) + h
-        c = self.group.hash(hash_input, ZR)
-
-        #g_A to u (scaled XT)
-        g_uTA = []
-        RBc = []
-        k = len(u) - 1
-        for j1 in range(k + 1):
-            for j2 in range(k):
-                eq1 = g_A[j2]**u[j2][j1] * g ** u[k][j1]
-
-                eq2 = (B[j1][j2] ** c) *  R[j2] * R[-1]
-                if eq1 != eq2:# * R[j1]:
-                    print ("False", j1, j2)
-                    return False
-                # else:
-                #     print ("True ", j1, j2)
-
-        return True
 
 
-    def verifyPK(self, pp, pk, R, u):
-        e_gh = pp['e_g1_g2']
-        g_A = pp['g_1^A']
-        k = len(g_A)
 
-        e_gAh = [] #A
-        for i in range(k):
-            e_gAh.append(pair(g_A[i],pp['g_2']))
-        e_gAh.append(e_gh)
-
-        A = e_gAh
-        B = pk['e(g_1,g_2)^{tau^T A}']
-
-        h_x = self.group.hash([A,B], ZR)
-
-        hash_input = str(R) + str(h_x)
-        c = self.group.hash(hash_input, ZR)
-        #c = 1
-        #print ('c:',c)
-
-        for i in range(k):
-            eq1 = e_gAh[i] ** u[i] * e_gh ** u[k]
-            eq2 = B[i] ** c * R[i] * R[k]
-            if eq1 != eq2:
-                print ("False", i)
-                return False
-            
-        return True
 
 class s_pair:
     def __init__(self, s, A, group, B = None):
